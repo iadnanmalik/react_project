9eb5c26f80ddf0394537019bf33afbd2958d0a08
iadnanmalik <61354025+iadnanmalik@users.noreply.github.com>
2020-06-10 16:55:20 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function Welcome(props) {
+  return
+  {
+    <div>
+      <h1>
+        Hello World
+      </h1>
+      <h2>
+        Lets start react
+      </h2>
+      </div>
+  }
+
+
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
